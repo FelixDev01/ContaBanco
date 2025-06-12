970a16732e4a8430426d54ae07f0114782956f30
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2025-06-11 22:00:53 -0300

    BancoTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,23 @@
-
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá, este é seu banco digital! \nDigite seu nome:");
+        String nome = scanner.next();
+        System.out.printf("Bom, %s, escreva os dados que vamos pedir porfavor.\n", nome);
+        System.out.println("Digite o Numero da Conta: ");
+        Integer conta = scanner.nextInt();
+        System.out.println("Digite o Numero da Agencia: ");
+        String agencia = scanner.next();
+        System.out.println("Digite o Nome completo do cliente: ");
+        String nomeConta = scanner.next();
+        System.out.println("Digite o Saldo da Conta: ");
+        Double saldo = scanner.nextDouble();
+        scanner.close();
+        System.out.println("========================================================================================================================================");
+        System.out.printf("Olá %s, obrigado por criar sua conta no nosso banco! A sua conta é %s. A sua agencia é %s. Seu saldo disponivel é R$ %s", nome, conta, agencia, saldo);
 
 
     }
